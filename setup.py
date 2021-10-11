38db7047fc80b9ac5df4d4aa87dff60792c71cb6
Julio Sánchez Jiménez <jsnchzjmnz@gmail.com>
2021-10-11 15:31:18 +0000

    Release 0.20.1a6

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ def _dbt_psycopg2_name():
 
 
 package_name = "dbt-cratedb"
-package_version = "0.20.1a5"
+package_version = "0.20.1a6"
 description = """The crate adpter plugin for dbt (data build tool)"""
 dbt_version = '0.20.0'
 
