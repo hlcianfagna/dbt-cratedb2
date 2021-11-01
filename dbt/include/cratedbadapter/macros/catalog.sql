25c7b74906088b60808b0949595a56a45fc30a9c
Julio Sánchez Jiménez <jsnchzjmnz@gmail.com>
2021-11-01 22:43:18 +0000

    Release 0.20.1a76

--- dbt/include/cratedbadapter/macros/catalog.sql
+++ dbt/include/cratedbadapter/macros/catalog.sql
@@ -51,10 +51,11 @@
           upper(sch.nspname) = upper('{{ schema }}'){%- if not loop.last %} or {% endif -%}
         {%- endfor -%}
       )
-      and tbl.relpersistence = 'p' -- [p]ermanent table. Other values are [u]nlogged table, [t]emporary table
-      and tbl.relkind in ('r', 'v', 'f', 'p') -- o[r]dinary table, [v]iew, [f]oreign table, [p]artitioned table. Other values are [i]ndex, [S]equence, [c]omposite type, [t]OAST table, [m]aterialized view
-      and col.attnum > 0 -- negative numbers are used for system columns such as oid
-      and not col.attisdropped order by sch.nspname, tbl.relname, col.attnum
+      and tbl.relpersistence = 'p'
+      and tbl.relkind in ('r', 'v', 'f', 'p')
+      and col.attnum > 0
+      and not col.attisdropped
+      order by sch.nspname, tbl.relname, col.attnum
 
   {%- endcall -%}
 
