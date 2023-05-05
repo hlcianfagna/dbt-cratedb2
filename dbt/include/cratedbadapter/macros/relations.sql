0c15979e00ac7bfa71ba0d55a7835c84ba69e33b
hlcianfagna <110453267+hlcianfagna@users.noreply.github.com>
2023-05-05 08:53:19 +0100

    Handle the lack of pg_depend and pg_rewrite

--- dbt/include/cratedbadapter/macros/relations.sql
+++ dbt/include/cratedbadapter/macros/relations.sql
@@ -1,3 +1,7 @@
-{% macro cratedb_get_relations () -%}
-  {{ return(dbt.postgres__get_relations()) }}
+{% macro postgres_get_relations () -%}
+  {%- call statement('relations', fetch_result=True) -%}
+    select 'mock' as referenced_schema,'referenced ' as referenced_name,
+        'mock' as dependent_schema,'dependent' as dependent_name;
+  {%- endcall -%}
+  {{ return(load_result('relations').table) }}
 {% endmacro %}
