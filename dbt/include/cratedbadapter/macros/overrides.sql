0bad385da450cbba03832ea8b217f77274ba0e91
hlcianfagna <110453267+hlcianfagna@users.noreply.github.com>
2023-05-30 13:58:21 +0100

    Recreate views instead of adding indirection
    First draft

--- dbt/include/cratedbadapter/macros/overrides.sql
+++ dbt/include/cratedbadapter/macros/overrides.sql
@@ -29,7 +29,7 @@
 
 {% macro default__drop_relation(relation) -%}
   {% call statement('drop_relation', auto_begin=False) -%}
-    drop {{ relation.type }} if exists {{ relation|replace('"crate".', "") }} 
+    drop {{ relation.type }} if exists "{{ relation.schema }}"."{{ relation.identifier }}"
   {%- endcall %}
 {% endmacro %}
 
@@ -43,14 +43,26 @@
   {%- set sql_header = config.get('sql_header', none) -%}
 
   {{ sql_header if sql_header is not none }}
-  create view {{ relation }} as 
+  create view "{{ relation.schema }}"."{{ relation.identifier }}" as 
     {{ sql|replace('"crate".', "") }}
   ;
 {%- endmacro %}
 
 {% macro postgres__rename_relation(from_relation, to_relation) -%}
-  {% call statement('rename_relation') -%}
-        create or replace view {{ to_relation.name|replace('"crate".', "") }} as 
-        select * from {{ from_relation|replace('"crate".', "") }}
-  {%- endcall %}
+  {% do drop_relation(to_relation) %}
+  {% set relation_type = run_query("select table_type from information_schema.tables where table_schema = '{{ from_relation.schema }}' and table_name = '{{ from_relation.identifier }}'") %}
+  {% if relation_type == 'VIEW' %}    
+    {% set view_definition = run_query("SELECT view_definition FROM information_schema.views WHERE table_schema = '{{ from_relation.schema }}' AND table_name = '{{ from_relation.identifier }}'") %}
+	{% call statement('drop_view') -%}
+      DROP VIEW IF EXISTS {{ to_relation.schema }}.{{ to_relation.identifier }};
+    {%- endcall %}
+    {% call statement('create_view') -%}
+      CREATE VIEW {{ to_relation.schema }}.{{ to_relation.identifier }} AS {{ view_definition }}
+    {%- endcall %}
+  {% else %}
+    {% call statement('rename_table') -%}
+      ALTER TABLE {{ from_relation.schema }}.{{ from_relation.identifier }}
+      RENAME TO {{ to_relation.schema }}.{{ to_relation.identifier }}
+    {%- endcall %}
+  {% endif %}
 {% endmacro %}
