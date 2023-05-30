e4c9c8fda5d4a232825fa7dc692661f76e650ed9
hlcianfagna <110453267+hlcianfagna@users.noreply.github.com>
2023-05-30 13:59:57 +0100

    Generate schema names correctly
    Adding code from https://discourse.getdbt.com/t/schema-name-is-not-correct/6816/3

--- dbt/include/cratedbadapter/macros/overrides.sql
+++ dbt/include/cratedbadapter/macros/overrides.sql
@@ -1,3 +1,18 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
+
 {% macro postgres__create_schema(relation) -%}
   {%- call statement('create_schema') -%}
     /* schemas are not created in CrateDB */
