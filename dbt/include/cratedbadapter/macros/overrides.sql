8008e3adc44cd6dfddbe0c98906a82fc535a5925
hlcianfagna <110453267+hlcianfagna@users.noreply.github.com>
2023-08-16 17:18:21 +0100

    Simple override of default__reset_csv_table with DELETE

--- dbt/include/cratedbadapter/macros/overrides.sql
+++ dbt/include/cratedbadapter/macros/overrides.sql
@@ -1,3 +1,16 @@
+{% macro default__reset_csv_table(model, full_refresh, old_relation, agate_table) %}
+    {% set sql = "" %}
+    {% if full_refresh %}
+        {{ adapter.drop_relation(old_relation) }}
+        {% set sql = create_csv_table(model, agate_table) %}
+    {% else %}
+        {{ adapter.truncate_relation(old_relation) }}
+        {% set sql = "delete from " ~ old_relation %}
+    {% endif %}
+
+    {{ return(sql) }}
+{% endmacro %}
+
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
