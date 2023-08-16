47630397262c63704e82976d9053bf3164ff3478
hlcianfagna <110453267+hlcianfagna@users.noreply.github.com>
2023-08-16 17:09:40 +0100

    postgres__list_relations_without_caching without pg_matviews
    https://github.com/dbt-labs/dbt-core/blob/ac539fd5cf325cfb5315339077d03399d575f570/plugins/postgres/dbt/include/postgres/macros/adapters.sql#L106

--- dbt/include/cratedbadapter/macros/relations.sql
+++ dbt/include/cratedbadapter/macros/relations.sql
@@ -5,3 +5,24 @@
   {%- endcall -%}
   {{ return(load_result('relations').table) }}
 {% endmacro %}
+
+{% macro postgres__list_relations_without_caching(schema_relation) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      '{{ schema_relation.database }}' as database,
+      tablename as name,
+      schemaname as schema,
+      'table' as type
+    from pg_tables
+    where schemaname ilike '{{ schema_relation.schema }}'
+    union all
+    select
+      '{{ schema_relation.database }}' as database,
+      viewname as name,
+      schemaname as schema,
+      'view' as type
+    from pg_views
+    where schemaname ilike '{{ schema_relation.schema }}'    
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
+{% endmacro %}
